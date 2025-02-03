1f00b0202b6c859efb8e6f91d91fff80104f32d8
WANDERERTRADER <141338909+WANDERERTRADER@users.noreply.github.com>
2025-02-03 18:05:06 +0700

    Update count_files.sh

--- cell00/ex03/count_files.sh
+++ cell00/ex03/count_files.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-file_count=$(find . -maxdepth 1 -type f | wc -l)
-
-dir_count=$(find . -maxdepth 1 -type d | wc -l)
-
-echo "Regular files: $file_count"
-echo "Directories: $dir_count"
+ls | wc -l
